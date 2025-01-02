96f95b049c69812c79f6a2e44ddb74c1c22adfdb
Lucas Colley <lucas.colley8@gmail.com>
2025-01-02 19:59:13 +0000

    DNM: import annotations

--- sklearn/externals/array_api_extra/_funcs.py
+++ sklearn/externals/array_api_extra/_funcs.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import warnings
 
 from ._lib import _compat, _utils
--- sklearn/externals/array_api_extra/_lib/_utils.py
+++ sklearn/externals/array_api_extra/_lib/_utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from . import _compat
 from ._typing import Array, ModuleType
 
